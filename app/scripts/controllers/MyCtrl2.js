e51f9a9b284b4f8b4fb2977a1c79aa39416aa9b6
Luca Lanziani <luca.lanziani@gmail.com>
2013-03-01 17:21:57 +0100

    small code improvements

---
+++ app/scripts/controllers/MyCtrl2.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+
+  var CONTROLLER_NAME = "MyCtrl2";
+
+  function controller($scope) {
+    $scope.name = CONTROLLER_NAME;
+  }
+
+  controller.$inject = ["$scope"];
+
+  angular
+    .module('myApp')
+    .controller(CONTROLLER_NAME, controller);
+
+})();
